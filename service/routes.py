3e923d466bd8298001bec85f696679e75584db09
viethoang1211 <viethoang12112002@gmail.com>
2023-09-23 00:59:28 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -62,7 +62,9 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
-@app.route("/accounts",methods=["GET"])
+
+
+@app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
     List all Accounts
@@ -71,12 +73,12 @@ def list_accounts():
     app.logger.info("Request to list Accounts")
 
     accounts = Account.all()
-    account_list =[]
+    account_list = []
     for i in accounts:
         account_list.append(i.serialize())
     # use the Account.all() method to retrieve all accounts
     # create a list of serialize() accounts
-    # log the number of accounts being returned in the list 
+    # log the number of accounts being returned in the list
     app.logger.info("Returning [%s] accounts", len(account_list))
     # return the list with a return code of status.HTTP_200_OK
     return jsonify(account_list), status.HTTP_200_OK
@@ -109,6 +111,7 @@ def get_accounts(account_id):
 
 # ... place you code here to UPDATE an account ...
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -120,7 +123,6 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
     account.deserialize(request.get_json())
     account.update()
 
@@ -136,6 +138,7 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
